2fbc128a0002ef1602bbe41314e9ae55051b3cf8
DreadPirateRobertsX <l.kurtiniak99@gmail.com>
2021-03-12 19:36:51 +0100

    zaciatok spracovavanie networku

--- MyProcesses.h
+++ MyProcesses.h
@@ -10,24 +10,26 @@ class  MyProcesses{
 
 public:
     void getProcesses();
-
+    void getNetworkConn();
     MyProcesses()
     {
         getProcesses();
         loadProcData();
+        getNetworkConn();
     }
 
     void printProcesses();
 private:
+    void formatUdpTcpTable(std::vector<std::vector<std::string> >& matrix);
+    void loadFileToVector(std::string fullPath,std::vector<std::vector<std::string> >& matrix);
     bool isNumber(std::string str);
     void loadProcData();
     static void loadLineToProcess(std::vector<std::string>& process,const std::string& fullPath, size_t lineNum);
     static void loadLineToProcess(std::vector<std::string> &process, const std::string &fullPath);
 
     std::vector<std::vector <std::string>> m_processes;
+    std::vector<std::vector <std::string>> m_networkConnections;
     std::string m_PID;
-
-
 };
 
 #endif //FORENSX_MYPROCESSES_H
--- Processes.cpp
+++ Processes.cpp
@@ -48,7 +48,7 @@ void MyProcesses::printProcesses() {
         std::cout << std::setw(6) << process[0] << std::setw(13) << process[1] << std::setw(26) << process[2] << std::setw(13) << process[3]<< std::setw(35) << process[4] << std::setw(40) << process[5] <<"\n";
     }
 }
-//test
+
 void MyProcesses::loadProcData() {
     std::string path = "/proc";
     std::ifstream file;
@@ -93,4 +93,68 @@ void MyProcesses::loadLineToProcess(std::vector<std::string>& process,const std:
     getline(file, line);
     process.push_back(line);
     file.close();
+}
+void MyProcesses::loadFileToVector(std::string fullPath,std::vector<std::vector<std::string>>& matrix) {
+
+    std::string line;
+    std::ifstream infile(fullPath);
+    std::vector<std::string> row;
+    int counter = 0;
+
+    while (std::getline(infile, line)) { //bude treba refactoring
+        std::string word;
+        std::istringstream tmp(line);
+        while (counter < line.size()) {
+            getline( tmp, word, ' ');
+            if(word != "")
+            {
+                row.push_back(word);
+                counter += word.size();
+            } else counter++;
+        }
+        counter = 0;
+        matrix.push_back(row);
+        row.clear();
+    }
+}
+
+void MyProcesses::getNetworkConn() {
+    std::string path = "/proc/net";
+    std::string fullPath;
+    std::vector<std::vector<std::string> > matrix;
+
+    fullPath = path + "/tcp";
+    loadFileToVector(fullPath,matrix);
+    formatUdpTcpTable(matrix);
+    std::cout << "test";
+}
+
+
+void MyProcesses::formatUdpTcpTable(std::vector<std::vector<std::string> >& matrix) {
+    std::string result;
+    matrix.erase(matrix.begin());
+    std::string tmp = "";
+    for (int j = 0; j < matrix.size(); ++j) {
+        for (int k = 1; k < 3; ++k) {
+            for (int i = 0; i < 13; i += 2) {
+                if (i == 8) {
+                    i--;
+                    continue;
+                }
+                std::string hexa(matrix[j][k], i, 2);
+                result = std::to_string(std::stol(hexa, nullptr, 16));
+                if (i < 8) {
+                    tmp.insert(0, result);
+                    if (i < 6)
+                        tmp.insert(0, ".");
+                } else {
+                    if (i == 9)
+                        tmp = tmp + ":";
+                    tmp = tmp + result;
+                }
+            }
+            matrix[j][k] = tmp;
+            tmp = "";
+        }
+    }
 }
